93944d6b7029fcc9ce4f3ac75b100beeaf9b8bdd
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2024-10-07 20:50:49 -0300

    clase 5

---
+++ Clase-5/script.js
@@ -0,0 +1,4 @@
+document.querySelector("#rect2").addEventListener("click",()=>{
+    document.querySelector("#rect2").style.fill="red";
+    document.querySelector("#rect2").style.animation="rotar 2s ";
+})
